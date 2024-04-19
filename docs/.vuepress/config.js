ff98bc54f81d8d9c5f098be03bb137ec9a00b9bc
user <1296800094@qq.com>
2024-04-19 10:54:11 +0800

    second commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+import {defineUserConfig} from 'vuepress'
+import {viteBundler} from '@vuepress/bundler-vite'
+import {plumeTheme} from 'vuepress-theme-plume'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    theme: plumeTheme({}),
+    bundler: viteBundler(),
+})
